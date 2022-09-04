51a06ea9b4f4fd1922e0930f6e780a6f80fdd7f2
DiegoTavelli <diegotavelli@gmail.com>
2022-09-04 15:35:08 -0300

    SearchPage

--- srcFrontOne/src/components/HomePage/HomePage.js
+++ srcFrontOne/src/components/HomePage/HomePage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
-// import SearchForm from "../SearchForm/SearchForm"
+import SearchForm from "../SearchForm/SearchForm"
 import styles from "./styles";
 
 const HomePage = () => {
     return (
         <View style={styles.container}>
-            {/* <SearchForm /> */}
+            <SearchForm />
         </View>
     )
 }
