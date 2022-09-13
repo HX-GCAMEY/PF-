f7e889429a35a00c4c2000ea6fefd1343da6d0dd
candela <candelatorrez99@gmail.com>
2022-09-13 20:58:09 -0300

    guardado

--- srcFrontOne/src/components/Authentication/Login/SocialButtons/SocialButtons.js
+++ srcFrontOne/src/components/Authentication/Login/SocialButtons/SocialButtons.js
@@ -4,6 +4,7 @@ import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { Image, View, Text } from "react-native";
 
+
 WebBrowser.maybeCompleteAuthSession();
 
 
@@ -12,8 +13,10 @@ const SocialButtons = ({navigation}) => {
     const [user, setUser] = useState(null);
     const [request, response, promptAsync] = Google.useAuthRequest({
         clientId: "803324446340-rru9e293vrsl0vku9te6lfcqp67j261d.apps.googleusercontent.com",
-        androidClientId: "803324446340-826d6c268hqd7i2jgtpcgf0jljol59pd.apps.googleusercontent.com"
+        androidClientId: "803324446340-826d6c268hqd7i2jgtpcgf0jljol59pd.apps.googleusercontent.com",
+        
     });
+
 
     useEffect(() => {
         if(response?.type === "success"){
@@ -30,21 +33,24 @@ const SocialButtons = ({navigation}) => {
         })
         const useInfo = await response.json();
         setUser(useInfo)
+       
     }
+
+    
 
     const ShowUserInfo = () => {
         if(user){
+            return(
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <Text style={{fontSize: 35, fontWeight: 'bold'}}>welcome</Text>
                 <Image source={{uri: user.picture}} style={{width: 100, height: 80}}/>
                 <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user.name}</Text>
             </View>
+
+            )
         }
     }
 
-    // const onLoginGoogle = () => {
-
-    // }
 
 
     return(
