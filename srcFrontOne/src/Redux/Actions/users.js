f256bbdb1ddac1d5f5ac9b18e30111d5b11eb28f
candela <candelatorrez99@gmail.com>
2022-09-14 21:42:51 -0300

    profile

--- srcFrontOne/src/Redux/Actions/users.js
+++ srcFrontOne/src/Redux/Actions/users.js
@@ -3,9 +3,31 @@ import axios from "axios";
 
 export function getUser(email){
     return async function(dispatch){
-        let res = await axios.get('https://flymatepf.herokuapp.com/api/users/findUser', email)
+        let res = await axios.get(`https://flymatepf.herokuapp.com/api/users/findUser/${email}`)
         dispatch({
             type: "GET_USER",
+            payload: res.data
+        })
+    }
+}
+
+
+export function updateUser (email, profile){
+    return async function(dispatch){
+        let res = await axios.put('https://flymatepf.herokuapp.com/api/users/updateUser', {email, profile})
+        dispatch({
+            type: 'UPDATE_USER',
+            payload: res.data
+        })
+    }
+}
+
+
+export function googleRegister(email){
+    return async function(dispatch){
+        let res = await axios.post('https://flymatepf.herokuapp.com/api/users/googleRegister', email)
+        dispatch({
+            type: 'GOOGLE_REGISTER',
             payload: res.data
         })
     }
