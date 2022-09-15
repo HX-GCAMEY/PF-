623bd5f705bc950a9bf0e36749ddb6e8b7d773d0
Gama <lic.camey@gmail.com>
2022-09-15 18:12:53 -0300

    Rutas para paquetes

--- srcBack/DAO/ticketsDAO.js
+++ srcBack/DAO/ticketsDAO.js
@@ -1,144 +1,226 @@
-import { ObjectId } from "bson";
+import {ObjectId} from "bson";
 
 let tickets;
+let packages;
 export default class TicketsDAO {
-    
-    static async injectDB(conn) {
-      if (tickets) {
-        return;
-      }
-      try {
-        tickets = await conn.db(process.env.FLYMATE_NS).collection("tickets");
-      } catch (e) {
-        console.error(`Unable to establish collection handles in userDAO: ${e}`);
-      }
+  static async injectDB(conn) {
+    if (tickets && packages) {
+      return;
     }
-
-    static async addNewFlightTicket(number, totalTickets){
-      try{
-        await tickets.insertOne(
-          {
-            flight_id: number,
-            totalTickets: totalTickets,
-            availableTickets: totalTickets
-          }
-        )
-        return {success: true};
-      }catch(e){
-        console.error(`Error occurred while adding new flight's ticket, ${error}.`);
-        return {error: error};
-      }
+    try {
+      tickets = await conn.db(process.env.FLYMATE_NS).collection("tickets");
+      packages = await conn.db(process.env.FLYMATE_NS).collection("packages");
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`);
     }
+  }
 
-    static async addFlightTicket({flight_id, user_id, type, fare}, availableTickets){
-      try {
-        await tickets.updateOne(
-          {
-            flight_id: flight_id,
-          },{ 
-            $set :
-            {
-              availableTickets: availableTickets -1
+  static async addPackage(
+    code,
+    startDate,
+    endDate,
+    origin,
+    destination,
+    airport,
+    price,
+    amount,
+    description
+  ) {
+    try {
+      await packages.insertOne({
+        code: code,
+        startDate: startDate,
+        endDate: endDate,
+        origin: origin,
+        destination: destination,
+        airport: airport,
+        price: price,
+        amount: amount,
+        description: description,
+      });
+      return {success: true};
+    } catch (error) {
+      console.error(
+        `Error occurred while adding new flight's ticket, ${error}.`
+      );
+      return {error: error};
+    }
+  }
+
+  static async addNewFlightTicket(number, totalTickets) {
+    try {
+      await tickets.insertOne({
+        flight_id: number,
+        totalTickets: totalTickets,
+        availableTickets: totalTickets,
+      });
+      return {success: true};
+    } catch (error) {
+      console.error(
+        `Error occurred while adding new flight's ticket, ${error}.`
+      );
+      return {error: error};
+    }
+  }
+
+  static async addFlightTicket(
+    {flight_id, user_id, type, fare},
+    availableTickets
+  ) {
+    try {
+      await tickets.updateOne(
+        {
+          flight_id: flight_id,
+        },
+        {
+          $set: {
+            availableTickets: availableTickets - 1,
+          },
+          $addToSet: {
+            tickets: {
+              _id: ObjectId(Math.random(1) * 1000000000)
+                .toString()
+                .replace(/[^0-9]/g, ""),
+              user_id: user_id,
+              type: type,
+              fare: fare,
             },
-            $addToSet:
-            {
-              tickets:                    
-                {
-                  _id: (ObjectId(Math.random(1)*1000000000)).toString().replace(/[^0-9]/g,''),
-                  user_id: user_id,
-                  type: type,
-                  fare: fare
-                }
-            }  
-          },{
-          upsert: true
-          }
-        );
-        return {success: true};
-      } catch (error) {
-        console.error(`Error occurred while adding new ticket, ${error}.`);
-        return {error: error};
-      }
+          },
+        },
+        {
+          upsert: true,
+        }
+      );
+      return {success: true};
+    } catch (error) {
+      console.error(`Error occurred while adding new ticket, ${error}.`);
+      return {error: error};
     }
+  }
 
-    static async getAvailableTicket(flightNum){
-      const flightTicket = await tickets.findOne({flight_id: flightNum});
-      return flightTicket.availableTickets;
+  static async getAvailableTicket(flightNum) {
+    const flightTicket = await tickets.findOne({flight_id: flightNum});
+    return flightTicket.availableTickets;
+  }
+
+  static async getAllPackages() {
+    try {
+      const allPackages = await packages.find().toArray();
+
+      return allPackages;
+    } catch (error) {
+      console.error(`Error occurred while retrieving comments, ${error}`);
+      return null;
     }
+  }
 
-    static async getFlightTicket(flightNum){
-      const flightTicket = await tickets.findOne({flight_id: flightNum});
-      if(flightTicket == null) return true;
-      return false
-    }
-
-    static async compareTicket(ticket_id){
+  static async getPackageByCode(code) {
+    try {
       const pipeline = [
         {
-          '$match': {
-            'tickets': {
-              '$elemMatch': {
-                '_id': ticket_id
-              }
-            }
-          }
-        }, {
-          '$project': {
-            '_id': 0, 
-            'availableTickets': 1
-          }
-        }
-      ];
-      if(!(await tickets.aggregate(pipeline).hasNext())) return false
-      return await tickets.aggregate(pipeline).next();
-    }
-
-    static async deleteTicket(ticket_id, availableTickets){
-      try{
-        await tickets.updateOne(
-          {
-            'tickets': {
-              '$elemMatch': {
-                '_id': ticket_id
-              }
-            }
-          },{
-            $set:{
-              "availableTickets": availableTickets + 1
-            },
-            $pull:{
-                "tickets": {
-                  "_id": ticket_id
-                }
-            }
-          });
-        return {success: true}
-      }catch(error){
-        console.error(`An error ocurred while deleting ticket ${error}`);
-        return {error: error};
-      }
-    };
-
-    static async getTicket(email){
-      const pipeline = [
+          $match: {
+            code: code,
+          },
+        },
         {
-          '$match': {
-            'tickets': {
-              '$elemMatch': {
-                'user_id': email
-              }
-            }
-          }
-        },{
-          '$project': {
-            '_id': 0,
-            'flight_id': 1,
-            'tickets._id': 1,
-            'tickets.type': 1,
-            'tickets.fare': 1
-          }
-        }
+          $project: {
+            _id: 0,
+            code: 1,
+            startDate: 1,
+            endDate: 1,
+            origin: 1,
+            destination: 1,
+            airport: 1,
+            price: 1,
+            amount: 1,
+            description: 1,
+          },
+        },
       ];
-      return await tickets.aggregate(pipeline).toArray()
-    };
+      return await packages.aggregate(pipeline).toArray();
+    } catch (error) {
+      console.error(`Error occurred while retrieving packages, ${error}`);
+      return null;
+    }
+  }
+
+  static async getFlightTicket(flightNum) {
+    const flightTicket = await tickets.findOne({flight_id: flightNum});
+    if (flightTicket == null) return true;
+    return false;
+  }
+
+  static async compareTicket(ticket_id) {
+    const pipeline = [
+      {
+        $match: {
+          tickets: {
+            $elemMatch: {
+              _id: ticket_id,
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          availableTickets: 1,
+        },
+      },
+    ];
+    if (!(await tickets.aggregate(pipeline).hasNext())) return false;
+    return await tickets.aggregate(pipeline).next();
+  }
+
+  static async deleteTicket(ticket_id, availableTickets) {
+    try {
+      await tickets.updateOne(
+        {
+          tickets: {
+            $elemMatch: {
+              _id: ticket_id,
+            },
+          },
+        },
+        {
+          $set: {
+            availableTickets: availableTickets + 1,
+          },
+          $pull: {
+            tickets: {
+              _id: ticket_id,
+            },
+          },
+        }
+      );
+      return {success: true};
+    } catch (error) {
+      console.error(`An error ocurred while deleting ticket ${error}`);
+      return {error: error};
+    }
+  }
+
+  static async getTicket(email) {
+    const pipeline = [
+      {
+        $match: {
+          tickets: {
+            $elemMatch: {
+              user_id: email,
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          flight_id: 1,
+          "tickets._id": 1,
+          "tickets.type": 1,
+          "tickets.fare": 1,
+        },
+      },
+    ];
+    return await tickets.aggregate(pipeline).toArray();
+  }
 }
--- srcBack/api/Routes/ticketsRouter.js
+++ srcBack/api/Routes/ticketsRouter.js
@@ -6,6 +6,9 @@ const router = new Router();
 router.route("/purchase").put(TicketsController.purchase);
 router.route("/cancel").put(TicketsController.cancel);
 router.route("/send/:email").get(TicketsController.send);
+router.route("/createPackage").post(TicketsController.createPackage);
+router.route("/allPackages").get(TicketsController.getPackages);
+router.route("/packageCode/:code").get(TicketsController.apiGetPackageByCode);
 // ruta pare enviar ticket por email
 
 export default router;
