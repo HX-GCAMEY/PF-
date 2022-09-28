3ecda031e77b88a2d4b9e097f60b7fa31dcd2052
Gama <lic.camey@gmail.com>
2022-09-28 14:10:23 -0300

    Cambios en estilo

--- srcFrontOne/src/components/Profile/Inputs/Inputs.js
+++ srcFrontOne/src/components/Profile/Inputs/Inputs.js
@@ -1,83 +1,86 @@
 import React, {useState} from "react";
-import { View, Text, Button, Image,  TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import {useDispatch, useSelector} from "react-redux";
-import { updateUser } from "../../../Redux/Actions/users";
+import {updateUser} from "../../../Redux/Actions/users";
 import {LinearGradient} from "expo-linear-gradient";
 import styles from "./styles";
 import logoProfile from "../imgs/logoProfile.png";
 
 const Inputs = ({navigation}) => {
-    const [name, setName] = useState('');
-    const [passport, setPassport] = useState('');
-    const [nationality, setNationality] = useState('');
-    const [phone, setPhone] = useState('');
+  const [name, setName] = useState("");
+  const [passport, setPassport] = useState("");
+  const [nationality, setNationality] = useState("");
+  const [phone, setPhone] = useState("");
 
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.userReducer.session)
-    console.log("aca toy en input", user)
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userReducer.session);
+  console.log("aca toy en input", user);
 
+  const handleSubmit = (name, passport, nationality, phone) => {
+    dispatch(updateUser(user.email, {name, passport, nationality, phone}));
+    alert("To ensure your safety, we ask that you log in again");
+    navigation.navigate("Profile");
+    setName("");
+    setPassport("");
+    setNationality("");
+    setPhone("");
+  };
 
-    const handleSubmit = (name, passport, nationality, phone) => {
-        dispatch(updateUser(user.email, {name, passport, nationality, phone}))
-        alert('To ensure your safety, we ask that you log in again')
-        navigation.navigate('Profile')
-        setName('');
-        setPassport('');
-        setNationality('');
-        setPhone('');
-        
-    }
+  return (
+    <LinearGradient colors={["#06C5C5", "#14366F"]} style={{height: "100%"}}>
+      <View
+        style={{alignItems: "center", marginTop: 120, paddingHorizontal: 10}}
+      >
+        <Image source={logoProfile} />
 
+        <Text style={styles.textInput}>{"Name:"}</Text>
+        <TextInput
+          style={styles.inputs}
+          value={name}
+          onChangeText={(name) => setName(name)}
+          placeholder="Insert name..."
+        />
 
-    return(
-    <LinearGradient colors={['#07C5C5', '#0184A0']} style={{height: '100%'}}>
-        <View style={{alignItems: 'center', marginTop: 120, paddingHorizontal: 10}}>
-            <Image source={logoProfile} />
+        <Text style={styles.textInput}>{"Passport:"}</Text>
+        <TextInput
+          style={styles.inputs}
+          value={passport}
+          onChangeText={(passport) => setPassport(passport)}
+          placeholder="Insert passport..."
+        />
 
-            
-            <Text style={styles.textInput}>{'Name:'}</Text>
-            <TextInput 
-            style={styles.inputs}
-            value={name}
-            onChangeText={(name) => setName(name)}
-            placeholder="Insert name..."
-            />
-    
-            <Text style={styles.textInput}>{'Passport:'}</Text>
-            <TextInput
-            style={styles.inputs}             
-            value={passport}
-            onChangeText={(passport) => setPassport(passport)}
-            placeholder="Insert passport..."
-            />
-    
-            <Text style={styles.textInput}>{'Nationality:'}</Text>
-            <TextInput 
-            style={styles.inputs}
-            value={nationality}
-            onChangeText={(nationality) => setNationality(nationality)}
-            placeholder="Insert nationality..."
-            />
-    
-            <Text style={styles.textInput}>{'Phone:'}</Text>
-            <TextInput 
-            style={styles.inputs}           
-            value={phone}
-            onChangeText={(phone) => setPhone(phone)}
-            placeholder="Insert phone number..."
-            />
+        <Text style={styles.textInput}>{"Nationality:"}</Text>
+        <TextInput
+          style={styles.inputs}
+          value={nationality}
+          onChangeText={(nationality) => setNationality(nationality)}
+          placeholder="Insert nationality..."
+        />
 
-            <TouchableOpacity  onPress={() => handleSubmit(name, passport, nationality, phone)} style={styles.btnS}>
-                <Text style={styles.btnSubmit}>SUBMIT</Text>
-            </TouchableOpacity>
-              
-            
+        <Text style={styles.textInput}>{"Phone:"}</Text>
+        <TextInput
+          style={styles.inputs}
+          value={phone}
+          onChangeText={(phone) => setPhone(phone)}
+          placeholder="Insert phone number..."
+        />
 
-    </View>
+        <TouchableOpacity
+          onPress={() => handleSubmit(name, passport, nationality, phone)}
+          style={styles.btnS}
+        >
+          <Text style={styles.btnSubmit}>SUBMIT</Text>
+        </TouchableOpacity>
+      </View>
     </LinearGradient>
-
-    )
-}
-
+  );
+};
 
 export default Inputs;
--- srcFrontOne/src/components/Profile/Profile.js
+++ srcFrontOne/src/components/Profile/Profile.js
@@ -1,194 +1,257 @@
-import React, { useEffect, useState } from "react";
-import { View, Image, Button, Text, TouchableOpacity, Modal, TextInput, ScrollView, Pressable } from "react-native";
+import React, {useEffect, useState} from "react";
+import {
+  View,
+  Image,
+  Button,
+  Text,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+  ScrollView,
+  Pressable,
+} from "react-native";
 import styles from "./styles";
-import avatar from "./imgs/avatar.png"
+import avatar from "./imgs/avatar.png";
 import * as ImagePicker from "expo-image-picker";
 import Feather from "react-native-vector-icons/Feather";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { useDispatch, useSelector } from "react-redux";
-import { LinearGradient } from "expo-linear-gradient";
+import {useDispatch, useSelector} from "react-redux";
+import {LinearGradient} from "expo-linear-gradient";
 import welcome from "./imgs/welcome.png";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
-import { userLogout, userDelete } from "../../Redux/Actions/users";
+import {userLogout, userDelete} from "../../Redux/Actions/users";
 import FlatListReviews from "../Review/FlatListReviews";
 import MyTickets from "./MyTickets/MyTickets";
 //import Categories from "./categories/categories";
 
+const Profile = ({navigation}) => {
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const user = useSelector((state) => state.userReducer.session);
+  const categories = ["Profile", "My Tickets", "Reviews"];
+  const [categoryIndex, setCategoryIndex] = useState(0);
+  const dispatch = useDispatch();
 
-const Profile = ({ navigation }) => {
-
-
-    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
-    const [image, setImage] = useState(null);
-    const user = useSelector((state) => state.userReducer.session);
-    const categories = ["Profile", "My Tickets", "Reviews"];
-    const [categoryIndex, setCategoryIndex] = useState(0);
-    const dispatch = useDispatch();
-
-
-    const CategoryList = () => {
-        return (
-            <View style={styles.categoryContainer2}>
-                {categories.map((item, index) => (
-                    <TouchableOpacity key={index} onPress={() => setCategoryIndex(index)}>
-                        <Text style={[styles.categoryText2, categoryIndex === index && styles.categoryTextSelected2]}>{item}</Text>
-                    </TouchableOpacity>
-                ))}
-            </View>
-        )
-    }
-
-
-
-
-    useEffect(() => {
-        (async () => {
-            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            setHasGalleryPermission(galleryStatus.status === 'granted');
-        })()
-    }, [])
-
-
-
-    const pickImage = async () => {
-        let res = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-        })
-        if (!res.cancelled) {
-            setImage(res.uri)
-        }
-    }
-    if (hasGalleryPermission === false) {
-        return (
-            <Text>No access to Internal Storage</Text>
-        )
-    }
-
-
-
-    const onLogout = () => {
-        if (user.email) {
-            dispatch(userLogout(user.email))
-        }
-        navigation.navigate("Login")
-        alert("Session closed successfully")
-    }
-
-
-    const changePassword = () => {
-        navigation.navigate("ForgotPassword")
-    }
-
-
-    const onEdit = () => {
-        navigation.navigate("EditProfile")
-    }
-
-
-    const onDelete = () => {
-        if (user.email) {
-            dispatch(userDelete(user))
-        }
-        alert('Account deleted successfully')
-    }
-
-    const TicketsContainer = () => {
-        return(
-            <View style={styles.ticketContainer}>
-                <MyTickets email={user.email} />
-            </View>
-        );
-    }
-
-    const ProfileCategory = () => {
-        return (
-            <View>
-                <View style={{ alignItems: 'center' }}>
-                    <Image source={welcome} style={{ marginTop: 40, marginLeft: 18, height: 30 }} />
-                    {user.email ? <Text style={{ fontWeight: 'bold', fontSize: 18, marginTop: 10 }}>{user.email}</Text> : <Text style={{ color: '#131c46', fontSize: 18, fontWeight: 'bold', marginLeft: 10 }}>user@email.com</Text>}
-
-                </View>
-
-                <View style={styles.textInfo}>
-                    <Feather name="user" size={25} style={styles.iconsP} />
-                    {user.profile?.name ? <Text style={styles.textCat}>{user.profile.name}</Text> : <Text style={styles.textCat}>Full Name</Text>}
-                </View>
-
-                <View style={styles.textInfo}>
-                    <FontAwesome5 name="passport" size={25} style={styles.iconsP} />
-                    {user.profile?.passport ? <Text style={styles.textCat}>{user.profile.passport}</Text> : <Text style={styles.textCat}>Passport</Text>}
-                </View>
-
-                <View style={styles.textInfo}>
-                    <FontAwesome5 name="map-marker-alt" size={25} style={styles.iconsP} />
-                    {user.profile?.nationality ? <Text style={styles.textCat}>{user.profile.nationality}</Text> : <Text style={styles.textCat}>Nationality</Text>}
-                </View>
-
-                <View style={styles.textInfo}>
-                    <FontAwesome5 name="phone-square-alt" size={25} style={styles.iconsP} />
-                    {user.profile?.phone ? <Text style={styles.textCat}>{user.profile.phone}</Text> : <Text style={styles.textCat}>Phone number</Text>}
-                </View>
-
-                <View>
-                    <TouchableOpacity style={styles.editBtn} onPress={() => onEdit()}>
-                        <Text style={styles.editProfile}>Edit profile</Text>
-                    </TouchableOpacity>
-                    <MaterialCommunityIcons
-                        name="logout"
-                        size={40}
-                        style={{ marginLeft: 180, marginTop: 30 }}
-                        onPress={() => onLogout()}
-                    />
-                </View>
-                <View style={styles.changeBtn}>
-                    <MaterialCommunityIcons name="onepassword" size={25} style={{ marginRight: 15 }} />
-                    <Text style={{ fontSize: 16, marginLeft: -5, color: 'black' }} onPress={() => changePassword()}>Change password</Text>
-                </View>
-                <TouchableOpacity style={styles.deleteBtn} onPress={() => onDelete()}>
-                    <Text style={{ color: 'red', fontSize: 15, fontWeight: 'bold' }}>Delete account</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-
+  const CategoryList = () => {
     return (
-        <ScrollView>
-            <LinearGradient colors={['#07C5C5', '#028DA4']} style={{ height: '100%' }}>
-                <Pressable style={styles.containerProfile} onPress={() => pickImage()}>
-                    {image ?
-                        <View style={styles.containerImage}>
-                            <Image source={{ uri: image }} style={styles.imgUser} />
-                        </View>
+      <View style={styles.categoryContainer2}>
+        {categories.map((item, index) => (
+          <TouchableOpacity key={index} onPress={() => setCategoryIndex(index)}>
+            <Text
+              style={[
+                styles.categoryText2,
+                categoryIndex === index && styles.categoryTextSelected2,
+              ]}
+            >
+              {item}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    );
+  };
 
-                        : <View style={styles.containerImage}>
-                <Image source={avatar} style={[styles.avatarImg, {width:110, height:110}]} />
-              </View>
-              }
-            <MaterialIcons name="add-a-photo" color={'#072f4e'} size={25} style={{marginLeft: 220, marginTop: -20}}/>
+  useEffect(() => {
+    (async () => {
+      const galleryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let res = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!res.cancelled) {
+      setImage(res.uri);
+    }
+  };
+  if (hasGalleryPermission === false) {
+    return <Text>No access to Internal Storage</Text>;
+  }
+
+  const onLogout = () => {
+    if (user.email) {
+      dispatch(userLogout(user.email));
+    }
+    navigation.navigate("Login");
+    alert("Session closed successfully");
+  };
+
+  const changePassword = () => {
+    navigation.navigate("ForgotPassword");
+  };
+
+  const onEdit = () => {
+    navigation.navigate("EditProfile");
+  };
+
+  const onDelete = () => {
+    if (user.email) {
+      dispatch(userDelete(user));
+    }
+    alert("Account deleted successfully");
+  };
+
+  const TicketsContainer = () => {
+    return (
+      <View style={styles.ticketContainer}>
+        <MyTickets email={user.email} />
+      </View>
+    );
+  };
+
+  const ProfileCategory = () => {
+    return (
+      <View>
+        <View style={{alignItems: "center"}}>
+          <Image
+            source={welcome}
+            style={{marginTop: 40, marginLeft: 18, height: 30}}
+          />
+          {user.email ? (
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 20,
+                marginTop: 10,
+                color: "white",
+              }}
+            >
+              {user.email}
+            </Text>
+          ) : (
+            <Text
+              style={{
+                color: "#131c46",
+                fontSize: 18,
+                fontWeight: "bold",
+                marginLeft: 10,
+              }}
+            >
+              user@email.com
+            </Text>
+          )}
+        </View>
+
+        <View style={styles.textInfo}>
+          <Feather name="user" size={25} style={styles.iconsP} />
+          {user.profile?.name ? (
+            <Text style={styles.textCat}>{user.profile.name}</Text>
+          ) : (
+            <Text style={styles.textCat}>Full Name</Text>
+          )}
+        </View>
+
+        <View style={styles.textInfo}>
+          <FontAwesome5 name="passport" size={25} style={styles.iconsP} />
+          {user.profile?.passport ? (
+            <Text style={styles.textCat}>{user.profile.passport}</Text>
+          ) : (
+            <Text style={styles.textCat}>Passport</Text>
+          )}
+        </View>
+
+        <View style={styles.textInfo}>
+          <FontAwesome5 name="map-marker-alt" size={25} style={styles.iconsP} />
+          {user.profile?.nationality ? (
+            <Text style={styles.textCat}>{user.profile.nationality}</Text>
+          ) : (
+            <Text style={styles.textCat}>Nationality</Text>
+          )}
+        </View>
+
+        <View style={styles.textInfo}>
+          <FontAwesome5
+            name="phone-square-alt"
+            size={25}
+            style={styles.iconsP}
+          />
+          {user.profile?.phone ? (
+            <Text style={styles.textCat}>{user.profile.phone}</Text>
+          ) : (
+            <Text style={styles.textCat}>Phone number</Text>
+          )}
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.editBtn} onPress={() => onEdit()}>
+            <Text style={styles.editProfile}>Edit profile</Text>
+          </TouchableOpacity>
+          <MaterialCommunityIcons
+            name="logout"
+            size={40}
+            style={{marginLeft: 180, marginTop: 30}}
+            onPress={() => onLogout()}
+          />
+        </View>
+        <View style={styles.changeBtn}>
+          <MaterialCommunityIcons
+            name="onepassword"
+            size={25}
+            style={{marginRight: 15}}
+          />
+          <Text
+            style={{fontSize: 16, marginLeft: -5, color: "black"}}
+            onPress={() => changePassword()}
+          >
+            Change password
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.deleteBtn} onPress={() => onDelete()}>
+          <Text style={{color: "red", fontSize: 15, fontWeight: "bold"}}>
+            Delete account
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return (
+    <ScrollView>
+      <LinearGradient colors={["#06C5C5", "#14366F"]} style={{height: "100%"}}>
+        <Pressable style={styles.containerProfile} onPress={() => pickImage()}>
+          {image ? (
+            <View style={styles.containerImage}>
+              <Image source={{uri: image}} style={styles.imgUser} />
+            </View>
+          ) : (
+            <View style={styles.containerImage}>
+              <Image
+                source={avatar}
+                style={[styles.avatarImg, {width: 110, height: 110}]}
+              />
+            </View>
+          )}
+          <MaterialIcons
+            name="add-a-photo"
+            color={"#072f4e"}
+            size={25}
+            style={{marginLeft: 220, marginTop: -20}}
+          />
         </Pressable>
         <View>
-            <CategoryList />
-            <View style={{height: '100%'}} >
-            {
-                !user 
-                    ? <Text>Loading...</Text> 
-                    : categoryIndex === 0 
-                        ? <ProfileCategory/>
-                        : categoryIndex === 1
-                            ? <TicketsContainer />
-                            : categoryIndex === 2
-                                ? <FlatListReviews />
-                                : null
-            }
-              </View>
+          <CategoryList />
+          <View style={{height: "100%"}}>
+            {!user ? (
+              <Text>Loading...</Text>
+            ) : categoryIndex === 0 ? (
+              <ProfileCategory />
+            ) : categoryIndex === 1 ? (
+              <TicketsContainer />
+            ) : categoryIndex === 2 ? (
+              <FlatListReviews />
+            ) : null}
+          </View>
         </View>
-        </LinearGradient>
-        </ScrollView>
-    )
-}
+      </LinearGradient>
+    </ScrollView>
+  );
+};
 
 export default Profile;
